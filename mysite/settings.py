29003a57b5af8b0bd8cc97c87e265879999b60f8
Arbind_Mehta <samsungmom67@gmail.com>
2020-04-27 16:54:06 +0545

    STATIC

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS=[
 os.path.join(BASE_DIR, 'static')
 ]
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
