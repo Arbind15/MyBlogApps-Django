8a6150e5e27cdc36200f0a2bfbc327e7788f7f37
Arbind_Mehta <samsungmom67@gmail.com>
2020-04-28 14:00:34 +0545

    Add Release Phase static

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +125,10 @@ STATICFILES_DIRS=[
 os.path.join(BASE_DIR, 'static')
 ]
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
