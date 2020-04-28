5c68baf88ee755ab0caa61f7383aacc26d2865d6
Arbind_Mehta <samsungmom67@gmail.com>
2020-04-28 10:28:41 +0545

    Add

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,9 +80,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-'OPTIONS': {
-        'isolation_level': psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE,
     }
 }
 
